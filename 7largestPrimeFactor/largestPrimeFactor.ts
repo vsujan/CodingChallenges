7ab2c50f8072409f47848ce29330cd51c536eafb
Sujan Vaidya <shujan1992@gmail.com>
2024-11-11 19:21:12 +0545

    code for largestPrimeFactor

---
+++ 7largestPrimeFactor/largestPrimeFactor.ts
@@ -0,0 +1,27 @@
+/*
+Write a program that finds the largest prime factor of a given positive integer n.
+where 2 ≤ 𝑛 ≤ 10^9
+
+Example:
+Input: 13195
+5
+2639
+7
+377
+13
+29
+Output: 29
+
+Input: 600851475143
+Output: 6857
+*/
+
+const largestPrimeFactor = (num: number): number => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      num = num / i;
+      i = 2;
+    }
+  }
+  return num;
+};
