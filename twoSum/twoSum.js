df0d0d20f4f94182e819ca84d8d858a866fd8431
Sujan Vaidya <shujan1992@gmail.com>
2024-11-11 19:01:26 +0545

    add code for twoSum

---
+++ twoSum/twoSum.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+Given an array of integers, return the indices of the two numbers such that they add up to a specific target.
+
+Input:nums = [2, 7, 11, 15], target = 9
+Output: [0, 1]  # Because nums[0] + nums[1] = 2 + 7 = 9
+*/
+const twoSum = (nums, target) => {
+    const refObj = {};
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (refObj[complement] !== undefined) {
+            result = [refObj[complement], i];
+        }
+        refObj[nums[i]] = i;
+    }
+    return result.length === 2 ? result : undefined;
+};
